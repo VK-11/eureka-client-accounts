5303321f6d8026fcc50717bbec54e0765197671f
Vikas Kaushik <vikas_kaushik@epam.com>
2021-07-29 11:24:41 +0530

    initial commit

---
+++ src/main/java/com/bank/eurekaclientaccounts/EurekaClientAccountsApplication.java
@@ -0,0 +1,13 @@
+package com.bank.eurekaclientaccounts;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EurekaClientAccountsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EurekaClientAccountsApplication.class, args);
+	}
+
+}
